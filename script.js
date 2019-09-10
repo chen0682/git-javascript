ef6b0b160246c884b34eadae8f774872b2b7143d
chen0682 <chen0682@algonquinlive.com>
2019-09-10 13:17:42 -0400

    Adds the Template Literals code.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const name = 'Fang Chen'
-console.log('Hello, my name is ' + name + '.')
-// console.log(`Hello, my name is ${name}`.) Do the same thing in another way.
+// console.log('Hello, my name is ' + name + '.') Do the same thing in another way.
+console.log(`Hello, my name is ${name}.`)
 
 const number = 6
 console.log(number * 2 * 5 / number - 7)
