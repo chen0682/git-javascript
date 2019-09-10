802d18863b7184ab369e10e175e3013444cb1958
chen0682 <chen0682@algonquinlive.com>
2019-09-10 12:28:14 -0400

    Adds comments.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const name = 'Fang Chen'
 console.log('Hello, my name is' + name)
-// console.log(`Hello, my name is ${name}`.)
+// console.log(`Hello, my name is ${name}`.) Do the same thing in another way.
 
 const number = 6
 console.log(number * 2 * 5 / number - 7)
