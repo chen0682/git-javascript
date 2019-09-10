b70aebe87dcc248b82e325a2ff76bd21a222462d
chen0682 <chen0682@algonquinlive.com>
2019-09-10 12:34:56 -0400

    Adds a period.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const name = 'Fang Chen'
-console.log('Hello, my name is ' + name)
+console.log('Hello, my name is ' + name + '.')
 // console.log(`Hello, my name is ${name}`.) Do the same thing in another way.
 
 const number = 6
